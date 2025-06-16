f03de9b99d0bfa1dd3d5e14f2d7185c29f07ee38
eloibernier <eloi.bernier1@gmail.com>
2025-06-16 03:42:43 +0000

    global, and Markowitz

---
+++ InvestmentPortfolioAI/notebooks/DescriptiveAnalysisPortfoli.ipynb_checkpoints/globals.py
@@ -0,0 +1,30 @@
+# Set globale variables for the portfolio analysis
+
+# Define the amount invested in each asset
+Asset_Amounts = {
+    "AAPL": 30000,
+    "GOOGL": 3000,
+    "AMZN": 4000,
+    "MSFT": 3500,
+    "TSLA": 2500,
+    "NFLX": 2000,
+    "NVDA": 4500,
+    "V": 3000,
+    "QQQ": 0,
+    "SPY": 0
+}
+
+
+#Define the amount invested in each asset
+OptimizedPortfolio = {
+    "AAPL": 1500,
+    "GOOGL": 3000,
+    "AMZN": 4000,
+    "MSFT": 3500,
+    "TSLA": 50000,
+    "NFLX": 2000,
+    "NVDA": 4500,
+    "V": 50000,
+    "QQQ": 0,
+    "SPY": 5000
+}
